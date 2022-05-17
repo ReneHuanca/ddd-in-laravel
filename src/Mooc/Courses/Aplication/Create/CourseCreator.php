b4c14d7e1db7d82c58c33e4b93942a97845fa3c8
ReneHuanca <renehuanca999@gmail.com>
2022-05-17 10:54:14 -0400

    solve the id problem with add named constructor register

--- src/Mooc/Courses/Aplication/Create/CourseCreator.php
+++ src/Mooc/Courses/Aplication/Create/CourseCreator.php
@@ -7,7 +7,6 @@ namespace CodelyTv\Mooc\Courses\Aplication\Create;
 use App\Http\Requests\CreateCourseRequest;
 use CodelyTv\Mooc\Courses\Domain\Course;
 use CodelyTv\Mooc\Courses\Domain\CourseDuration;
-use CodelyTv\Mooc\Courses\Domain\CourseId;
 use CodelyTv\Mooc\Courses\Domain\CourseName;
 use CodelyTv\Mooc\Courses\Domain\CourseRepository;
 
@@ -22,11 +21,10 @@ final class CourseCreator
 
     public function __invoke(CreateCourseRequest $request)
     {
-        $id       = new CourseId("3");
         $name     = new CourseName($request->name);
         $duration = new CourseDuration($request->duration);
 
-        $course = Course::create($id, $name, $duration);
+        $course = Course::register($name, $duration);
 
         $this->repository->save($course);
     }
